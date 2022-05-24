01d5e5dc1d328c93ec64f9834c94696e62e7cd3b
karth <karthiknaveen22022002@gmail.com>
2022-05-24 21:21:13 +0530

    added ace python editor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import React from 'react'
-
+import "./App.css"
 import styled from "styled-components"
+
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+
+
+
+
+
+const MainContainer = styled.div`
+display: flex;
+flex-direction: column;
+height: 100vh;
+width: 100vw;
+
+`
+
+
+
+const Navbar = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+  width: 100vw;
+  height: 70px;
+  background-color: violet;
+`;
 
 const Container = styled.div`
 display: flex;
@@ -10,6 +39,8 @@ height: 100vh;
 
 
 `
+
+
 const PythonFile = styled.div`
 display: flex;
 flex: 13;
@@ -29,6 +60,8 @@ const IpOpContainer = styled.div`
 `;
 
 
+
+
 const Input = styled.div`
 display: flex;
 flex: 1;
@@ -36,6 +69,15 @@ background-color: green;
 height: 50vh;
 
 `
+
+
+const InputTextArea = styled.textarea`
+  height: 100%;
+  width: 100%;
+`;
+
+
+
 
 const Output = styled.div`
 display: flex;
@@ -47,28 +89,45 @@ height:50vh;
 
 
 const App = () => {
+
+
+
+  function onChange(newValue) {
+    console.log("change", newValue);
+  }
+
+
+
   return (
-    <Container>
-      <PythonFile>
-        
-
-      </PythonFile>
-      <IpOpContainer>
-        <Input>
+    <MainContainer>
+      <Navbar>
+        <h1>Python Compiler</h1>
+      </Navbar>
+      <Container>
+        <PythonFile>
+          <AceEditor
+            mode="python"
+            theme="github"
+            onChange={onChange}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+            width="100%"
+            height="100%"
+            fontSize={18}
           
-        </Input>
-        <Output>
-
-        </Output>
-
-        
-      </IpOpContainer>
-
-
-
-
-    </Container>
-  )
+          >
+            
+          </AceEditor>
+        </PythonFile>
+        <IpOpContainer>
+          <Input>
+            <InputTextArea></InputTextArea>
+          </Input>
+          <Output></Output>
+        </IpOpContainer>
+      </Container>
+    </MainContainer>
+  );
 }
 
 export default App
