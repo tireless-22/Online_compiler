d4d061c47f545c1273e285211521f95665e85f74
karth <karthiknaveen22022002@gmail.com>
2022-05-25 00:01:50 +0530

    backup1

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,30 @@
 const { c, cpp, node, python, java } = require("compile-run");
 const express = require("express")
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 
 
+
+// app.post("/python", (req, res) => {
+// 	const sourcecode = `print("Hell0 W0rld!")
+// 	print("this is knk)`;
+//   let resultPromise = python.runSource(sourcecode);
+//   resultPromise
+//     .then((result) => {
+//       console.log(result);
+//     })
+//     .catch((err) => {
+//       console.log("Hello");
+//     });
+// })
 
 
 app.post("/python", (req, res) => {
-	const sourcecode = `print("Hell0 W0rld!")
-	print("this is knk)`;
-  let resultPromise = python.runSource(sourcecode);
+ 
+  let resultPromise = python.runFile(req.body.formData);
   resultPromise
     .then((result) => {
       console.log(result);
@@ -17,10 +32,10 @@ app.post("/python", (req, res) => {
     .catch((err) => {
       console.log("Hello");
     });
+});
 
-	
-	
-})
+
+
 
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,128 +1,127 @@
-import React from 'react'
-import "./App.css"
-import styled from "styled-components"
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
 
 import AceEditor from "react-ace";
-
-
+import axios from "axios"
+import { useState } from "react";
 
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
 
-import {VscRunAll} from "react-icons/vsc"
-
+import { VscRunAll } from "react-icons/vsc";
 
 const MainContainer = styled.div`
-display: flex;
-flex-direction: column;
-height: 100vh;
-width: 100vw;
-
-`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+`;
 
 const Navbar = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100vw;
   height: 70px;
   background-color: violet;
 `;
 
-
 const NameHeader = styled.div`
-display: flex;
-height: 40px;
-width: 100%;
-justify-content: center;
-align-items: center;
-
-
-`
-
+  display: flex;
+  height: 40px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Container = styled.div`
-display: flex;
-width: 100vw;
-height: 100vh;
-
-
-
-`
-
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
 
 const PythonFile = styled.div`
-display: flex;
-flex: 13;
-background-color: red;
-flex-direction: column;
-
-
-
-
-`
+  display: flex;
+  flex: 13;
+  background-color: red;
+  flex-direction: column;
+`;
 
 const IpOpContainer = styled.div`
   display: flex;
   flex: 7;
   background-color: blue;
-  flex-direction:column;
   flex-direction: column;
-
+  flex-direction: column;
 `;
 
-
-
-
 const Input = styled.div`
-display: flex;
-flex: 1;
-background-color: green;
-height: 50vh;
-
-`
-
+  display: flex;
+  flex: 1;
+  background-color: green;
+  height: 50vh;
+`;
 
 const InputTextArea = styled.textarea`
   height: 100%;
   width: 100%;
 `;
 
-
-
-
 const Output = styled.div`
-display: flex;
-flex: 1;
-background-color: yellow;
-height:50vh;
+  display: flex;
+  flex: 1;
+  background-color: yellow;
+  height: 50vh;
 
-flex-direction: column;
-`
-
-
+  flex-direction: column;
+`;
 
 const App = () => {
+ 
+
+  const [data, setData] = useState("Hello");
+  // const [fileName, setFileName] = useState("");
 
 
+  // function onChange(e) {
+  //   setData(e.target.value);
+  // }
+  
+  
+function onChange(newValue) {
+  console.log("change", newValue);
+}
 
-  function onChange(newValue) {
-    console.log("change", newValue);
-  }
+  // const saveFile = (e) => {
+  //   setData(e.target.value);
+  //   // setFile(e.target.files[0]);
+  //   // setFileName(e.target.files[0].name);
+  // };
 
-
-  function runPython(){
-    
-  }
-
-
+  const uploadFile = async (e) => {
+    // const formData = new FormData();
+    // formData.append("file",data);
+    // formData.append("filename","temp.py");
+    try {
+      // console.log(formData)
+      console.log(data);
+      const res = await axios.post("http://localhost:3000/python", data);
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
 
   return (
     <MainContainer>
       <Navbar>
         <h1>Python Compiler</h1>
-        <VscRunAll color='green' size={20}/>
+        <VscRunAll color="green" size={20} onClick={() => {
+          uploadFile()
+          
+        }}/>
       </Navbar>
       <Container>
         <PythonFile>
@@ -145,12 +144,11 @@ const App = () => {
           </Input>
           <Output>
             <NameHeader>Output</NameHeader>
-
           </Output>
         </IpOpContainer>
       </Container>
     </MainContainer>
   );
-}
+};
 
-export default App
+export default App;
